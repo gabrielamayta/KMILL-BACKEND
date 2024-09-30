a0317c224f4d495805032092f9cdef554a3c6980
mica <mica28368@gmail.com>
2024-09-30 12:10:00 -0300

    conexion a la base datos con MYSQL

---
+++ pruebas/conexion_mika.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+conexion = mysql.connector.connect(user='kmill', password='kmill111',
+                                    host='10.9.120.5', 
+                                    database='kmill')
+
+print(conexion)
+
+
+sqlSelect = """SELECT id, Nombre, stock, id_categoria
+           FROM Producto
+           ORDER BY id DESC
+           LIMIT 2"""
+
+cursor = conexion.cursor()
+
+cursor.execute(sqlSelect)
+
+resultadoSQL = cursor.fetchall()
+
+cursor.close()
+conexion.close()
+
+print (resultadoSQL)
