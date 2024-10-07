6ee8488fa8ca60b383df5f6354b280df4adea8ec
gabrielamayta <gabrielamayta2109@gmail.com>
2024-10-07 13:32:33 -0300

    hice las rutas de mi tabla (usuario_rol), pero no funciona todavia

--- pruebas/pruebagabi.py
+++ pruebas/pruebagabi.py
@@ -1,28 +1,33 @@
 import mysql.connector
- 
+from flask import Flask,jsonify
+
 #Conexión con el servidor MySQL Server
-conexionMySQL = mysql.connector.connect(
-    host='10.9.120.5',
-    user='kmill',
-    passwd='kmill111',
-    db='kmill'
-)
+
+app = Flask(__name__)
+#@app.route("/Usuario_rol/<int:id>")
  
+def Usua_rol(id): 
 #Consulta SQL que ejecutaremos, en este caso un select
-sqlSelect = """SELECT id, id_usuario, id_rol
-           FROM Usuario_rol
-           ORDER BY id DESC
-          """
-#Establecemos un cursor para la conexión con el servidor MySQL
-cursor = conexionMySQL.cursor()
-#A partir del cursor, ejecutamos la consulta SQL
-cursor.execute(sqlSelect)
-#Guardamos el resultado de la consulta en una variable
-resultadoSQL = cursor.fetchall()
- 
-#Cerramos el cursor y la conexión con MySQL
-cursor.close()
-conexionMySQL.close()
- 
-#Mostramos el resultado por pantalla
-print (resultadoSQL)
+    conexionMySQL = mysql.connector.connect(
+        host='10.9.120.5',
+        user='kmill',
+        passwd='kmill111',
+        db='kmill'
+    )
+    sqlSelect = """SELECT id, id_usuario, id_rol FROM Usuario_rol WHERE id = %s """
+    #Establecemos un cursor para la conexión con el servidor MySQL
+    cursor = conexionMySQL.cursor(dictionary=True)
+    #A partir del cursor, ejecutamos la consulta SQL
+    cursor.execute(sqlSelect,(id,))
+    #Guardamos el resultado de la consulta en una variable
+    resultadoSQL = cursor.fetchone()
+    
+    #Cerramos el cursor y la conexión con MySQL
+    cursor.close()
+    conexionMySQL.close()
+    
+    #Mostramos el resultado por pantalla
+    return jsonify(resultadoSQL)
+print("??")
+
+print(app.url_map)
